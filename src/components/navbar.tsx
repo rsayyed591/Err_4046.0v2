6bcf8b6e12317a6e63fa2be966ea94e25f8f000e
rsayyed591 <rehansayyed591@gmail.com>
2024-12-24 01:35:44 +0530

    added vercel.json

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -231,7 +231,8 @@ const ListItem = React.forwardRef<
     <li>
       <NavigationMenuLink asChild>
       <button
-  type="button" // Explicitly specify the type
+
+  type="button"
   onClick={onClick}
   className={cn(
     "block w-full text-left select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
